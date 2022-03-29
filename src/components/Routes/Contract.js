40da9ace561de82be11d685109245fcd877c50a7
iNorthIsle <ndobox@outlook.com>
2022-03-29 13:41:23 +0800

    chore: update

--- src/components/Routes/Contract.js
+++ src/components/Routes/Contract.js
@@ -1,1 +1,8 @@
-export { default } from '@obsidians/contract'
+import Contract from '@obsidians/contract'
+
+Contract.defaultProps = {
+	...Contract.defaultProps,
+	valueFormatter: value => value,
+}
+
+export default Contract
