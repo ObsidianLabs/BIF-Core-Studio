428f0c09fbb21b634caf614d2e4b6d1829706b0b
iNorthIsle <ndobox@outlook.com>
2022-03-30 14:49:56 +0800

    chore: update

--- src/components/Routes/BottomBar.js
+++ src/components/Routes/BottomBar.js
@@ -1,1 +1,19 @@
-export { default } from '@obsidians/bottombar'
+
+   
+import React from 'react'
+
+import { connect } from '@obsidians/redux'
+import BottomBar from '@obsidians/bottombar'
+
+function BottomBarWithProps ({ network, queue, uiState }) {
+  const localNetwork = uiState.get('localNetwork')
+  let txs
+  if (network !== 'dev') {
+    txs = queue.getIn([network, 'txs'])
+  } else if (localNetwork && localNetwork.lifecycle === 'started') {
+    txs = queue.getIn([localNetwork.params.id, 'txs'])
+  }
+  return <BottomBar secretName='Private Key' txs={txs} />
+}
+
+export default connect(['queue', 'network', 'uiState'])(BottomBarWithProps)
