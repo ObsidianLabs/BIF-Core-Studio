2daf0d0e4b0a02a3f9bbab5b1cfed7290fb66787
iNorthIsle <ndobox@outlook.com>
2022-05-30 08:31:12 +0800

    feat: bif provider

--- src/components/Routes/UserHomepage.js
+++ src/components/Routes/UserHomepage.js
@@ -21,16 +21,16 @@ class UserHomepage extends BaseUserHomepage {
   }
 
   async setNetwork (networkId, counter = 0) {
-    // if (networkManager?.networks?.length) {
-    //   const network = networkManager.networks.find(network => network.id === networkId)
-    //   if (network) {
-    //     networkManager.setNetwork(network, { redirect: false, notify: true })
-    //   }
-    //   this.props.history.replace(`/${this.props.match.params.username}`)
-    // } else if (counter <= 30) {
-    //   await new Promise(resolve => setTimeout(resolve, 1000))
-    //   await this.setNetwork(networkId, ++counter)
-    // }
+    if (networkManager?.networks?.length) {
+      const network = networkManager.networks.find(network => network.id === networkId)
+      if (network) {
+        networkManager.setNetwork(network, { redirect: false, notify: true })
+      }
+      this.props.history.replace(`/${this.props.match.params.username}`)
+    } else if (counter <= 30) {
+      await new Promise(resolve => setTimeout(resolve, 1000))
+      await this.setNetwork(networkId, ++counter)
+    }
   }
 }
 
