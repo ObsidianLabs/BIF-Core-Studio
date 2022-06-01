70565a8e2ffdf7515af5f5a5a5ba7642999efb34
北屿一木 <20736452+iNorthIsle@users.noreply.github.com>
2022-06-01 09:02:35 +0800

    feat: update bif studio (#4)

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,32 +3,19 @@ import platform from '@obsidians/platform'
 import { connect } from '@obsidians/redux'
 
 import headerActions, { Header, NavGuard } from '@obsidians/header'
-import { networkManager } from '@obsidians/network'
+import { networkManager } from '@obsidians/eth-network'
 import { actions } from '@obsidians/workspace'
-import keypairManager, { KeypairInputSelector } from '@obsidians/keypair'
+import keypairManager from '@obsidians/keypair'
 
 import { List } from 'immutable'
-import ConfluxSdk from '@obsidians/bif-sdk'
+import BifSdk from '@obsidians/bif-sdk'
 
 Header.defaultProps = {
   noUser: !platform.isWeb
 }
 
-const extraContractItems = [
-  { header: 'internal contracts' },
-  { id: '0x0888000000000000000000000000000000000000', name: 'AdminControl' },
-  { id: '0x0888000000000000000000000000000000000001', name: 'SponsorWhitelistControl' },
-  { id: '0x0888000000000000000000000000000000000002', name: 'Staking' },
-  { divider: true },
-]
-
-const prefix = {
-  tethys: 'cfx:',
-  testnet: 'cfxtest:',
-}
-
-keypairManager.kp = ConfluxSdk.kp
-networkManager.addSdk(ConfluxSdk, ConfluxSdk.networks)
+keypairManager.kp = BifSdk.kp
+networkManager.addSdk(BifSdk, BifSdk.networks)
 
 class HeaderWithRedux extends PureComponent {
   state = {
@@ -39,19 +26,11 @@ class HeaderWithRedux extends PureComponent {
     actions.history = this.props.history
     headerActions.history = this.props.history
     this.setState({ networkList: List(networkManager.networks) }, this.setNetwork)
-    if (!networkManager.network) {
+
+    if (!networkManager.network || !networkManager.sdk) {
       networkManager.setNetwork(networkManager.networks[0], { notify: false })
     }
     this.navGuard = new NavGuard(this.props.history)
-  }
-
-  componentDidUpdate (props) {
-    if (this.props.network !== props.network) {
-      const network = this.props.network
-      KeypairInputSelector.defaultProps = {
-        // filter: k => k.address?.startsWith(prefix[network] || '0x')
-      }
-    }
   }
 
   groupedNetworks = networksByGroup => {
@@ -86,8 +65,6 @@ class HeaderWithRedux extends PureComponent {
     const selectedContract = contracts.getIn([network, 'selected']) || ''
     const selectedAccount = accounts.getIn([network, 'selected']) || ''
 
-    const keypairManagerFilter = k => k.id.startsWith(prefix[network] || '0x')
-
     return (
       <Header
         profile={profile}
@@ -99,7 +76,6 @@ class HeaderWithRedux extends PureComponent {
         starredContracts={starredContracts}
         // keypairManagerFilter={keypairManagerFilter}
         browserAccounts={browserAccounts}
-        extraContractItems={extraContractItems}
         network={selectedNetwork}
         networkList={groupedNetworks}
       />
